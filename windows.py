4046938c0755ba5b17972d82042f1a40d723cf3f
cassette-axd <reyescaseym@gmail.com>
2023-11-23 14:45:10 -0600

    stored login data to json file if input is valid

--- windows.py
+++ windows.py
@@ -2,6 +2,7 @@
 from tkinter import *
 from tkinter import messagebox
 import re
+import json
 
 #first page to open up where users can sign in to their account
 class LoginWindow:
@@ -73,9 +74,54 @@ class CreateAccountWindow:
         save_info_button = Button(top, text="Create Account", width=28, command=self.submit)
         save_info_button.grid(column=1, row=5)
 
+    def save_info(self):
+        stored_accounts = {
+            self.username.get(): {
+                "Email": self.email.get(),
+                "Password":  self.password1.get()
+            }
+        }
+        # store data as a json file
+        try:
+            with open("data.json", "r") as data_file:
+                # read old data
+                data = json.load(data_file)
+        # create a new json data file if it doesn't currently exist
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(stored_accounts, data_file, indent=4)
+        else:
+            # update old data with new data
+            data.update(stored_accounts)
+
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=4)
+        # clear text boxes regardless of what happens
+        finally:
+            messagebox.showinfo(title="success", message="Your New Account Has Been Created")
+            self.username_entry.delete(0, END)
+            self.email_entry.delete(0, END)
+            self.password_entry.delete(0, END)
+            self.password_entry_2.delete(0, END)
+
     def submit(self):
+        # first check if account information already exists
+        try:
+            with open("data.json", "r") as data_file:
+                # read data
+                data = json.load(data_file)
+        except FileNotFoundError:
+                pass
+        else:
+            # check if input is stored in data
+            if self.username.get() in data:
+                messagebox.showinfo(title="Error", message="Error""\nUsername already exists")
+            for user, user_data in data.items():
+                if user_data['Email'] == self.email.get():
+                    messagebox.showinfo(title="Error", message="Error""\nEmail already exists")
+            
         regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-        # first check if given ivalid input
+        # then check if given ivalid input
         if self.password1.get() != self.password2.get():
             messagebox.showinfo(title="Error", message="Error""\nPasswords don't match")
         elif self.username.get() == "" or self.email.get() == "" or self.password1.get() == "" or self.password2.get() == "":
@@ -84,7 +130,7 @@ class CreateAccountWindow:
             messagebox.showinfo(title="Error", message="Error""\nEmail is invalid")
         else:
             # if all input is valid, store it in a dictionary
-            print("All input is valid")
+            self.save_info()
         
 root = Tk()
 root.title("Budget Tracker")
